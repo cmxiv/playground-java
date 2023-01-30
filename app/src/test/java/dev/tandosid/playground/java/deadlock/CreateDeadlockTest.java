7332f8438ec27dff625b415bcb33b9d7bcdc1dee
cmxiv <cmxiv914@gmail.com>
2023-01-30 12:54:18 +0100

    Disable create-deadlock test

--- app/src/test/java/dev/tandosid/playground/java/deadlock/CreateDeadlockTest.java
+++ app/src/test/java/dev/tandosid/playground/java/deadlock/CreateDeadlockTest.java
@@ -1,9 +1,11 @@
 package dev.tandosid.playground.java.deadlock;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
+@Disabled("This will generate a deadlock while running tests")
 class CreateDeadlockTest {
 
     @Test
